395fc3aa76f94449255949a330fb08cd5bb8b6d7
ejjang2030 <ejjang2030@gmail.com>
2021-05-31 15:26:35 +0900

    added signup by email

--- app/src/main/java/com/emotionwave/chord/SignupActivity.java
+++ app/src/main/java/com/emotionwave/chord/SignupActivity.java
@@ -2,8 +2,11 @@ package com.emotionwave.chord;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
@@ -17,5 +20,14 @@ public class SignupActivity extends AppCompatActivity {
         // 리소스 가져오기 실습
         String str = getResources().getString(R.string.find_id);
         Log.d("test", str);
+
+        Button button = findViewById(R.id.signup_by_email);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, SignupEmailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
